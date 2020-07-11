f3c433fbd84bfd839e2d12332743bbaf1849ed71
wattyo0314 <wantyan-wattyo.0314@outlook.jp>
2020-07-11 15:08:44 +0900

    Define the corresponding action in the controller
    該当するアクションをコントローラーに定義しました。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,14 @@
 class MessagesController < ApplicationController
+  # GET  /groups/:group_id/messages
   def index
+  end
+
+  # POST /groups/:group_id/message
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to root_path, notice: "メッセージを送信しました。"
+      render :edit
+    end
   end
 end
